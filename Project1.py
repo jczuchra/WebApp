81b8832ebe64d3a19d90f68a480bcc21d4cde440
jczuchra <czuchra.jakub@gmail.com>
2019-06-22 13:48:28 +0200

    Add upload page

--- Project1.py
+++ Project1.py
@@ -1,12 +1,26 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.utils import secure_filename
+from werkzeug import SharedDataMiddleware
 import statistics
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = './uploads'
+ALLOWED_EXTENSIONS = set(['txt'])
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///formdata.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'True'
+
+app.add_url_rule('/uploads/<filename>', 'uploaded_file',
+                 build_only=True)
+app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+    '/uploads':  app.config['UPLOAD_FOLDER']
+})
 
 db = SQLAlchemy(app)
 
@@ -37,6 +51,31 @@ class Formdata(db.Model):
 
 db.create_all()
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return render_template('home.html')
+            #redirect(url_for('uploaded_file',
+                    #                filename=filename))
+    return render_template('home.html')
+
 
 @app.route('/')
 def home():
@@ -58,7 +97,6 @@ def thx():
     wystroj = request.form['wystroj']
     typ = request.form['typ']
     wyglad = request.form['wyglad']
-    print(request.form['fileToUpload'])
 
     fd=Formdata(imie, wiek, rodzaj, czestosc, kwota, like, wystroj, typ, wyglad)
     db.session.add(fd)
